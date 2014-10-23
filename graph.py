fc7c44d42eeff8b8c1fe005911633f2b9705bb3c
Arne Schlüter <aesthaddicts@gmail.com>
2014-10-23 19:28:12 +0200

    Rewrite graph.py again to use idiomatic python

--- graph.py
+++ graph.py
@@ -5,52 +5,52 @@
 class Vertex:
     def __init__(self,key):
         self.id = key
-        self.connectedTo = {}
+        self.connected_to = {}
 
     def add_neighbor(self,nbr,weight=0):
-        self.connectedTo[nbr] = weight
+        self.connected_to[nbr] = weight
 
     def __str__(self):
-        return str(self.id) + ' connected to: ' + str([x.id for x in self.connectedTo])
+        return str(self.id) + ' connected to: ' + str([x.id for x in self.connected_to])
 
     def get_connections(self):
-        return self.connectedTo.keys()
+        return self.connected_to.keys()
 
     def get_id(self):
         return self.id
 
     def get_weight(self,nbr):
-        return self.connectedTo[nbr]
+        return self.connected_to[nbr]
 
 class Graph:
     def __init__(self):
-        self.vertList = {}
-        self.numVertices = 0
+        self.vertex_list = {}
+        self.num_vertices = 0
 
     def add_vertex(self,key):
-        self.numVertices = self.numVertices + 1
+        self.num_vertices = self.num_vertices + 1
         newVertex = Vertex(key)
-        self.vertList[key] = newVertex
+        self.vertex_list[key] = newVertex
         return newVertex
 
     def get_vertex(self,n):
-        if n in self.vertList:
-            return self.vertList[n]
+        if n in self.vertex_list:
+            return self.vertex_list[n]
         else:
             return None
 
     def __contains__(self,n):
-        return n in self.vertList
+        return n in self.vertex_list
 
     def add_edge(self,f,t,cost=0):
-        if f not in self.vertList:
+        if f not in self.vertex_list:
             nv = self.addVertex(f)
-        if t not in self.vertList:
+        if t not in self.vertex_list:
             nv = self.addVertex(t)
-        self.vertList[f].addNeighbor(self.vertList[t], cost)
+        self.vertex_list[f].addNeighbor(self.vertex_list[t], cost)
 
     def get_vertices(self):
-        return self.vertList.keys()
+        return self.vertex_list.keys()
 
     def __iter__(self):
-        return iter(self.vertList.values())
+        return iter(self.vertex_list.values())
