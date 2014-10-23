233c12c28a6e0c0352dca8b6aa5a667edc265263
Arne Schlüter <aesthaddicts@gmail.com>
2014-10-23 18:19:12 +0200

    Fix Bug in frontier, so you can call get_next_url infinitely many times

--- frontier.py
+++ frontier.py
@@ -8,10 +8,11 @@ class Frontier:
         self.to_visit.append(page_url)
 
     def get_next_url(self):
-        # pop in python returns the last element of the list.
-        # to make it easier understandable we return the first one
-        next_url = self.to_visit[0]
-        self.to_visit = self.to_visit[1:]
+        if self.to_visit:
+            # pop in python returns the last element of the list.
+            # to make it easier understandable we return the first one
+            next_url = self.to_visit[0]
+            self.to_visit = self.to_visit[1:]
 
-        self.visited.append(next_url)
-        return next_url
+            self.visited.append(next_url)
+            return next_url
